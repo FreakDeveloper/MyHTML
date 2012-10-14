3f7b3b59b5533b1ec588163c7584ed03e950aa9d
Paul Jargielo <rzymo11@wp.pl>
2012-10-14 17:04:15 +0200

    Css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-type" content="pl"/>
+<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+<script language="JavaScript" type="text/javascript"></script>
+
+<title>MyTest</title>
+</head>
+
+<script type="text/javascript">
+function myFunction() {
+};
+</script>
+
+<body>
+	<div class = "all">
+		<div class="loginPlace">
+			<form id="formAlign" action="#" method="post" enctype="text/plain">
+				<p id="formText" ><label for="Login">Login</label><input type="text" id="name" /></p>
+				<p id="formText" ><label for="Password">Password</label><input type="text" id="e-mail" /></p>
+				<p class="submit"><input type="submit" value="Submit" /></p>	
+			</form>
+		</div>
+	</div>
+</body>
+</html>
